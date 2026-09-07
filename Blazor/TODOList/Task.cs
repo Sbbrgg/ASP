7817ea90768c653c6454dd7818a23688c36e6832
un <un@DESKTOP-JAU235M>
2026-09-07 19:51:22 +0300

    Migrations in Movies DONE

---
+++ Blazor/TODOList/Task.cs
@@ -0,0 +1,18 @@
+﻿namespace TODOList
+{
+	public class Task
+	{
+		public string Description { get; set; }
+		public bool DONE { get; set; }
+		public DateTime AssignTime { get; set; }
+		public override bool Equals(object? other)
+		{
+			return this.Description.Equals
+				(
+				(other as TODOList.Task).Description,
+				StringComparison.OrdinalIgnoreCase
+				);
+			//return this.Description.Equals((other as TODOList.Task).Description);
+		}
+	}
+}
