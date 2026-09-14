6578a553ea28954c71cf1d6260f24c38dbab336d
un <un@DESKTOP-JAU235M>
2026-09-14 18:54:24 +0300

    Component - Students in progress

--- Blazor/Academy/Models/Student.cs
+++ Blazor/Academy/Models/Student.cs
@@ -9,14 +9,14 @@ namespace Academy.Models
 		public int stud_id { get; set; }
 		public string last_name { get; set; }
 		public string first_name { get; set; }
-		public string middle_name { get; set; }
+		public string? middle_name { get; set; }
 		public DateTime? birth_date { get; set; }
-		public string email { get; set; }
-		public string phone { get; set; }
+		public string? email { get; set; }
+		public string? phone { get; set; }
 		[Column(TypeName = "IMAGE")]
-		public byte[] photo { get; set; }
+		public byte[]? photo { get; set; }
 		[ForeignKey(nameof(Group))]
-		public int group {  get; set; }
+		public int? group {  get; set; }
 
 	}
 }
